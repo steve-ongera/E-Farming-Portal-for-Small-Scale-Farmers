b01c9bd8f7c54aef872e2e7c42f74b086b637043
steve <gadafisteve001@gmail.com>
2025-10-02 13:36:40 +0300

    made updates

--- E_Farming_portal_for_small_scale_farmers/urls.py
+++ E_Farming_portal_for_small_scale_farmers/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_application.urls')),  # include your app routes
@@ -12,3 +13,11 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Custom error handlers
+handler404 = 'main_application.views.custom_404'
+handler500 = 'main_application.views.custom_500'
+handler403 = 'main_application.views.custom_403'
+handler400 = 'main_application.views.custom_400'
+
+
